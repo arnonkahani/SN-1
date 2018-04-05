0a238c58a00e1b011a81b04c8494d23a95ec974e
Your Name <you@example.com>
2018-04-05 14:19:30 +0300

    add all functionality

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args)throws Exception{
         api temp = new api();
         temp.load_graph("Karate.csv");
+        temp.calculate_clustering_coefficients();
+        temp.get_all_clustering_coefficients();
+        temp.get_average_clustering_coefficient();
     }
 }
--- src/api.java
+++ src/api.java
@@ -6,55 +6,100 @@ import java.util.*;
 
 public class api {
 
-    public int[][] graph;
+    private HashMap <Integer,ArrayList<Integer>> graph = new HashMap<>();
+    private ArrayList<Pair<Integer,Double>> clustering_coefficients = new ArrayList<>();
+    private double average_coefficients;
 
+    public HashMap<Integer, ArrayList<Integer>> getGraph() {
+        return graph;
+    }
+
+    public ArrayList<Pair<Integer, Double>> getClustering_coefficients() {
+        return clustering_coefficients;
+    }
+
+    public double getAverage_coefficients() {
+        return average_coefficients;
+    }
 
     public void load_graph (String path) throws Exception{
         final BufferedReader br = new BufferedReader(new InputStreamReader(api.class.getClassLoader().getResourceAsStream(path)));
         ArrayList<Pair<Integer,Integer>> nodeList = new ArrayList<>();
-        int maxVertices = 0;
         while(br.ready()){
             String pair = br.readLine();
             String[] vertices = pair.split(",");
             int ver1 = Integer.parseInt(vertices[0]);
             int ver2 = Integer.parseInt(vertices[1]);
             Pair<Integer,Integer> node = new Pair<>(ver1,ver2);
-            maxVertices  =  Math.max(Math.max(ver1, ver2),maxVertices);
             nodeList.add(node);
         }
-        initialize_graph(maxVertices+1 , nodeList);
-        System.out.println(graph);
+        initialize_graph(nodeList);
     }
 
     public void calculate_clustering_coefficients(){
-
+        double countNodes = 0;
+        double sumCoefficients =0;
+        for (Map.Entry<Integer,ArrayList<Integer>> entry : graph.entrySet()) {
+            countNodes++;
+            int node = entry.getKey();
+            ArrayList<Integer> neighbors = entry.getValue();
+            double k = neighbors.size();
+            double e = 0;
+            double coefficients = 0;
+            if (k>1) {
+                for (int i = 0; i < neighbors.size(); i++) {
+                    ArrayList<Integer> neighbors_of_neighbor = graph.get(neighbors.get(i));
+                    if (neighbors_of_neighbor!=null) {
+                        for (int j = 0; j < neighbors.size(); j++) {
+                            if (neighbors_of_neighbor.contains(neighbors.get(j))) {
+                                e++;
+                            }
+                        }
+                    }
+                }
+                coefficients = 2*e/(k*(k-1));
+                sumCoefficients += coefficients;
+            }
+            clustering_coefficients.add(new Pair<>(node,coefficients));
+        }
+        average_coefficients = sumCoefficients/countNodes;
     }
 
     public double get_average_clustering_coefficient(){
-        return -1;
+        return average_coefficients;
     }
 
     public double get_clustering_coefficient(int node_id){
+        for (int i=0; i<clustering_coefficients.size(); i++){
+            Pair<Integer,Double> nodePair = clustering_coefficients.get(i);
+            if (nodePair.getKey()==node_id){
+                return nodePair.getValue();
+            }
+        }
         return -1;
     }
 
-    /*
+
     public List<Pair<Integer,Double>> get_all_clustering_coefficients(){
-
+        Collections.sort(clustering_coefficients, (a, b) -> a.getValue() < b.getValue() ? -1 : a.getValue() == b.getValue() ? 0 : 1);
+        return clustering_coefficients;
     }
-    */
 
-    private void initialize_graph(int maxVertices ,  ArrayList<Pair<Integer,Integer>> nodeList){
-        graph = new int[maxVertices][maxVertices];
-        for (int i=0; i< maxVertices; i++){
-            for (int j=0; j< maxVertices; j++){
-                graph[i][j] = 0;
-            }
-        }
+
+    private void initialize_graph(ArrayList<Pair<Integer,Integer>> nodeList){
         for (int i=0; i< nodeList.size(); i++){
-            graph[nodeList.get(i).getKey()][nodeList.get(i).getValue()] = 1;
-            graph[nodeList.get(i).getValue()][nodeList.get(i).getKey()] = 1;
+            if (!graph.containsKey(nodeList.get(i).getKey())){
+                graph.put(nodeList.get(i).getKey(),new ArrayList<>());
+            }
+            if (!graph.containsKey(nodeList.get(i).getValue())){
+                graph.put(nodeList.get(i).getValue(),new ArrayList<>());
+            }
+            ArrayList neighbors1 =  graph.get(nodeList.get(i).getKey());
+            ArrayList neighbors2 =  graph.get(nodeList.get(i).getValue());
+            neighbors1.add(nodeList.get(i).getValue());
+            graph.put(nodeList.get(i).getKey(),neighbors1);
+            neighbors2.add(nodeList.get(i).getKey());
+            graph.put(nodeList.get(i).getValue(),neighbors2);
         }
-
     }
 }
